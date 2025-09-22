fae2bff5052ff3872a50e56c20529dd1ec6ff778
RJun010 <ricardo.junquera@educa.madrid.org>
2025-09-22 21:31:04 +0200

    Update Calculadora_angulo_timón.py

--- Calculadora_angulo_timón.py
+++ Calculadora_angulo_timón.py
@@ -1,6 +1,6 @@
 def calcular_excentricidad(porcentaje_necesario, radio_de_la_polea):
     excentricidad = (porcentaje_necesario * radio_de_la_polea)/100
-    return(f"tienes que alejar el eje de la polea {excentricidad}cm para ganar un {porcentaje_necesario}% de giro")
+    return(f"tienes que alejar el eje de la polea {excentricidad}mm para ganar un {porcentaje_necesario}% de giro")
 
 def calcular_porcentaje(recorrido_original, recorrido_disponible):
     porcentaje = (recorrido_original/recorrido_disponible - 1) * 100
@@ -9,13 +9,13 @@ def calcular_porcentaje(recorrido_original, recorrido_disponible):
 
 # cambia la variable excenticidad timón en onshape para obtener el diseño 3D 
 # link al proyecto: https://cad.onshape.com/documents/b7df1c6d75dbca150c312733/w/0a975e62208bc7a5049a8817/e/4e7948d3884854bfda58f8e1
-print("introduce todas las medidas con las mismas unidades")
+print("introduce todas las medidas en mm")
 recorrido_original = float(input("introduce el recorrido completo que tiene tu timón en el reposa: "))
 
 recorrido_disponible = float(input("introduce el recorrido disponible que tiene tu timón: "))
 
 porcentaje_necesario = calcular_porcentaje(recorrido_original,recorrido_disponible)
 
-radio_de_la_polea = float(input("introduce el radio actual de tu timón: "))
+radio_de_la_polea = 30.454
 
 print(calcular_excentricidad(porcentaje_necesario,radio_de_la_polea))
